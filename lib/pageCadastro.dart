a40f6846338dadbdfa6e94ee74d1ff5824ddb684
Heuller <heullergit@gmail.com>
2020-08-24 17:24:36 -0300

    terminado pagina com validacao

--- lib/pageCadastro.dart
+++ lib/pageCadastro.dart
@@ -1,7 +1,15 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 
 bool botaoCliente = true;
 bool botaoPrestador = false;
+
+String nome;
+String email;
+String senha;
+String servico;
+
+final _formKey = GlobalKey<FormState>();
 
 class PageCadastro extends StatefulWidget {
   @override
@@ -12,11 +20,13 @@ class _PageCadastroState extends State<PageCadastro> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       backgroundColor: Color(0xff324755),
       body: Padding(
         padding: const EdgeInsets.all(30.0),
         child: Center(
             child: Form(
+          key: _formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
@@ -30,6 +40,25 @@ class _PageCadastroState extends State<PageCadastro> {
               Column(
                 children: <Widget>[
                   TextFormField(
+                    style: TextStyle(color: Colors.white),
+                    validator: (value) {
+                      if (value.isEmpty) return "O campo é obrigatório";
+                    },
+                    onSaved: (newValue) => nome = newValue,
+                    decoration: InputDecoration(
+                        labelText: 'Nome',
+                        labelStyle: TextStyle(color: Colors.grey),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xffD97D54)))),
+                  ),
+                  TextFormField(
+                    style: TextStyle(color: Colors.white),
+                    onSaved: (newValue) => email = newValue,
+                    validator: (value) {
+                      if (value.isEmpty) return "O campo é obrigatório";
+                      if (!EmailValidator.validate(value))
+                        return "Email inválido";
+                    },
                     decoration: InputDecoration(
                         labelText: 'Email',
                         labelStyle: TextStyle(color: Colors.grey),
@@ -37,6 +66,14 @@ class _PageCadastroState extends State<PageCadastro> {
                             borderSide: BorderSide(color: Color(0xffD97D54)))),
                   ),
                   TextFormField(
+                    style: TextStyle(color: Colors.white),
+                    onSaved: (newValue) => senha = newValue,
+                    validator: (value) {
+                      if (value.isEmpty) return "O campo é obrigatório";
+                      if (value.length < 6)
+                        return "A senha precisa ter no mínimo 6 caracteres";
+                    },
+                    obscureText: true,
                     decoration: InputDecoration(
                         labelText: 'Senha',
                         labelStyle: TextStyle(color: Colors.grey),
@@ -46,6 +83,13 @@ class _PageCadastroState extends State<PageCadastro> {
                   Visibility(
                     visible: botaoPrestador,
                     child: TextFormField(
+                      style: TextStyle(color: Colors.white),
+                      onSaved: (newValue) => servico = newValue,
+                      validator: (value) {
+                        if (value.isEmpty) return "O campo é obrigatório";
+                        if (value.length < 6)
+                          return "A senha precisa ter no mínimo 6 caracteres";
+                      },
                       decoration: InputDecoration(
                           labelText: 'Serviço',
                           labelStyle: TextStyle(color: Colors.grey),
@@ -111,7 +155,9 @@ class _PageCadastroState extends State<PageCadastro> {
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(18.0),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      signUp();
+                    },
                     color: Colors.white,
                     textColor: Color(0xff324755),
                     child: Text("Próximo".toUpperCase(),
@@ -124,5 +170,9 @@ class _PageCadastroState extends State<PageCadastro> {
         )),
       ),
     );
+  }
+
+  void signUp() {
+    if (_formKey.currentState.validate()) {}
   }
 }
